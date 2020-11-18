372e9bef0977de6759cad2b4acc76eadcaa34e8e
Basit.Abdulkareem <Basit.Abdulkareem@ISW-ENGR-BA>
2020-11-18 20:25:24 +0100

    added staging build workflow on develop branch

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,9 @@ const HatsPage = () => (
 
 const App: React.FC = () => {
   console.log('>>>', process.env.NODE_ENV)
+  console.log('>>>', process.env)
+  console.log('>>>', JSON.stringify(process.env))
+
   return (
     <div>
         <Switch>
